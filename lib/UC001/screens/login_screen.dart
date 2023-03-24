a2259d46f89f1b5e9c14406a5f6ae2fd34ede3d0
KrittinK <kkamolpo@cmkl.ac.th>
2023-03-24 10:57:09 +0700

    Login_screen

--- lib/UC001/screens/login_screen.dart
+++ lib/UC001/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class NameScreen extends StatelessWidget {
-  const NameScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
