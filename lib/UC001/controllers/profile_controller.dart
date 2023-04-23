32ffeacd29cefa3f75e4f24ba37b6884172dd6a1
KrittinK <kkamolpo@cmkl.ac.th>
2023-04-24 00:15:04 +0700

    ProfileController
    Add more label to student

--- lib/UC001/controllers/profile_controller.dart
+++ lib/UC001/controllers/profile_controller.dart
@@ -24,4 +24,76 @@ class ProfileController {
 
     return studentId;
   }
+
+  Future<String?> getStudentClass({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.Class;
+    }
+
+    return studentId;
+  }
+
+  Future<String?> getStudentGPA({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.GPA;
+    }
+
+    return studentId;
+  }
+
+  Future<String?> getStudentCredit({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.Credit;
+    }
+
+    return studentId;
+  }
+
+  Future<String?> getStudentEmail({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.Email;
+    }
+
+    return studentId;
+  }
+
+  Future<String?> getStudentPhone({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.Phone;
+    }
+
+    return studentId;
+  }
+
+  Future<String?> getStudentAddress({
+    required String studentId,
+  }) async {
+    final studentDataList = await GetStudentDataService().getstudentdata();
+
+    for (final student in studentDataList) {
+      if (student.id == studentId) return student.Address;
+    }
+
+    return studentId;
+  }
 }
--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,15 +1,27 @@
 class Student {
   final String? id;
+  final String? GPA;
+  final String? Credit;
   final String? firstName;
   final String? lastName;
+  final String? Class;
+  final String? Email;
+  final String? Phone;
+  final String? Address;
 
-  Student({this.id, this.firstName, this.lastName});
+  Student({this.id, this.firstName, this.lastName, this.Class  , this.GPA, this.Credit, this.Email, this.Phone, this.Address});
 
   factory Student.fromJson(Map<String, dynamic> json) {
     return Student(
       id: json['id'],
       firstName: json['first_name'],
       lastName: json['last_name'],
+      Class: json['Class'],
+      GPA: json['GPA'],
+      Credit: json['Credit'],
+      Email: json['Email'],
+      Phone: json['Phone'],
+      Address: json['Address'],
     );
   }
 }
